07c1c3d1e2ebb7dc0deb7ac555494db34f3ec130
ROWAN <kimkj93@naver.com>
2024-09-24 16:34:11 +0900

    add: 프로젝트 생성

---
+++ PlateReminder/PlateReminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlateReminder
+//
+//  Created by Rowan on 9/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlateReminder/PlateReminder/PlateReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlateReminderApp.swift
+//  PlateReminder
+//
+//  Created by Rowan on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PlateReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
